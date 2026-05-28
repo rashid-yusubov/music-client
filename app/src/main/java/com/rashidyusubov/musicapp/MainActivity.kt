a13a6059c15260e72e0916fc8d1db204e3293338
Rashid Yusubov <rashidyusubov@yandex.ru>
2026-05-28 22:51:16 +0300

    chore: setup hilt application foundation

--- app/src/main/java/com/rashidyusubov/musicapp/MainActivity.kt
+++ app/src/main/java/com/rashidyusubov/musicapp/MainActivity.kt
@@ -4,44 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.rashidyusubov.musicapp.ui.theme.MusicTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
+
         setContent {
+
             MusicTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MusicTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/rashidyusubov/musicapp/MusicApp.kt
@@ -0,0 +1,7 @@
+package com.rashidyusubov.musicapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MusicApp : Application()
