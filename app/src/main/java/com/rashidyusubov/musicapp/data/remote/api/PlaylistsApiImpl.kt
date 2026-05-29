f1f32807e1f69e075e92ffbad12022d5d170b6ae
Rashid Yusubov <rashidyusubov@yandex.ru>
2026-05-29 23:34:38 +0300

    feat: add playlists data layer

---
+++ app/src/main/java/com/rashidyusubov/musicapp/data/remote/api/PlaylistsApiImpl.kt
@@ -0,0 +1,65 @@
+package com.rashidyusubov.musicapp.data.remote.api
+
+import com.rashidyusubov.musicapp.core.network.BASE_URL
+import com.rashidyusubov.musicapp.data.remote.dto.CreatePlaylistRequest
+import com.rashidyusubov.musicapp.data.remote.dto.PlaylistDto
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.delete
+import io.ktor.client.request.get
+import io.ktor.client.request.header
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
+import io.ktor.http.HttpHeaders
+import javax.inject.Inject
+
+class PlaylistsApiImpl @Inject constructor(
+    private val client: HttpClient
+) : PlaylistsApi {
+
+    override suspend fun getPlaylists(token: String): List<PlaylistDto> {
+
+        return client.get("${BASE_URL}playlists") {
+
+            header(HttpHeaders.Authorization, "Bearer $token") }.body()
+    }
+
+    override suspend fun createPlaylist(token: String, title: String, description: String?) {
+
+        client.post("${BASE_URL}playlists") {
+
+            header(HttpHeaders.Authorization, "Bearer $token")
+
+            setBody(
+                CreatePlaylistRequest(
+                    title = title,
+                    description = description
+                )
+            )
+        }
+    }
+
+    override suspend fun deletePlaylist(playlistId: Int, token: String) {
+
+        client.delete("${BASE_URL}playlists/$playlistId") {
+
+            header(HttpHeaders.Authorization, "Bearer $token")
+        }
+    }
+
+    override suspend fun addTrackToPlaylist(playlistId: Int, trackId: Int, token: String) {
+
+        client.post("${BASE_URL}playlists/$playlistId/tracks/$trackId") {
+
+            header(HttpHeaders.Authorization, "Bearer $token")
+        }
+    }
+
+    override suspend fun removeTrackFromPlaylist(playlistId: Int, trackId: Int, token: String) {
+
+        client.delete("${BASE_URL}playlists/$playlistId/tracks/$trackId") {
+
+            header(HttpHeaders.Authorization, "Bearer $token")
+        }
+    }
+}
--- app/src/main/java/com/rashidyusubov/musicapp/di/RepositoryModule.kt
+++ app/src/main/java/com/rashidyusubov/musicapp/di/RepositoryModule.kt
@@ -4,14 +4,18 @@ import com.rashidyusubov.musicapp.data.remote.api.AlbumsApi
 import com.rashidyusubov.musicapp.data.remote.api.AlbumsApiImpl
 import com.rashidyusubov.musicapp.data.remote.api.ArtistsApi
 import com.rashidyusubov.musicapp.data.remote.api.ArtistsApiImpl
+import com.rashidyusubov.musicapp.data.remote.api.PlaylistsApi
+import com.rashidyusubov.musicapp.data.remote.api.PlaylistsApiImpl
 import com.rashidyusubov.musicapp.data.remote.api.TracksApi
 import com.rashidyusubov.musicapp.data.remote.api.TracksApiImpl
 import com.rashidyusubov.musicapp.data.repository.AlbumsRepositoryImpl
 import com.rashidyusubov.musicapp.data.repository.ArtistsRepositoryImpl
+import com.rashidyusubov.musicapp.data.repository.PlaylistsRepositoryImpl
 import com.rashidyusubov.musicapp.data.repository.SearchHistoryRepositoryImpl
 import com.rashidyusubov.musicapp.data.repository.TracksRepositoryImpl
 import com.rashidyusubov.musicapp.domain.repository.AlbumsRepository
 import com.rashidyusubov.musicapp.domain.repository.ArtistsRepository
+import com.rashidyusubov.musicapp.domain.repository.PlaylistsRepository
 import com.rashidyusubov.musicapp.domain.repository.SearchHistoryRepository
 import com.rashidyusubov.musicapp.domain.repository.TracksRepository
 import dagger.Binds
@@ -43,4 +47,10 @@ abstract class RepositoryModule {
 
     @Binds
     abstract fun bindAlbumsApi(api: AlbumsApiImpl): AlbumsApi
+
+    @Binds
+    abstract fun bindPlaylistsRepository(repository: PlaylistsRepositoryImpl): PlaylistsRepository
+
+    @Binds
+    abstract fun bindPlaylistsApi(api: PlaylistsApiImpl): PlaylistsApi
 }
