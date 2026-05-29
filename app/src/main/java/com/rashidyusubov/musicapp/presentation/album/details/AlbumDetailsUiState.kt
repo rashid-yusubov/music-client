02b7652b8c2b1032570a7786a20b28ddcbe111e8
Rashid Yusubov <rashidyusubov@yandex.ru>
2026-05-29 18:57:20 +0300

    feat: add album details view model

---
+++ app/src/main/java/com/rashidyusubov/musicapp/presentation/album/details/AlbumDetailsUiState.kt
@@ -0,0 +1,15 @@
+package com.rashidyusubov.musicapp.presentation.album.details
+
+import com.rashidyusubov.musicapp.domain.model.Album
+import com.rashidyusubov.musicapp.domain.model.Track
+
+data class AlbumDetailsUiState(
+
+    val album: Album? = null,
+
+    val tracks: List<Track> = emptyList(),
+
+    val isLoading: Boolean = false,
+
+    val error: String? = null
+)
---
+++ app/src/main/java/com/rashidyusubov/musicapp/presentation/album/details/AlbumDetailsViewModel.kt
@@ -0,0 +1,67 @@
+package com.rashidyusubov.musicapp.presentation.album.details
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.rashidyusubov.musicapp.domain.usecase.GetAlbumByIdUseCase
+import com.rashidyusubov.musicapp.domain.usecase.GetAlbumTracksUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class AlbumDetailsViewModel @Inject constructor(
+    private val getAlbumByIdUseCase: GetAlbumByIdUseCase,
+    private val getAlbumTracksUseCase: GetAlbumTracksUseCase
+) : ViewModel() {
+
+    private val _state =
+        MutableStateFlow(
+            AlbumDetailsUiState()
+        )
+
+    val state =
+        _state.asStateFlow()
+
+    fun loadAlbum(
+        albumId: Int
+    ) {
+
+        viewModelScope.launch {
+
+            try {
+
+                _state.value =
+                    _state.value.copy(
+                        isLoading = true
+                    )
+
+                val album =
+                    getAlbumByIdUseCase(
+                        albumId
+                    )
+
+                val tracks =
+                    getAlbumTracksUseCase(
+                        albumId
+                    )
+
+                _state.value =
+                    _state.value.copy(
+                        album = album,
+                        tracks = tracks,
+                        isLoading = false
+                    )
+
+            } catch (e: Exception) {
+
+                _state.value =
+                    _state.value.copy(
+                        error = e.message,
+                        isLoading = false
+                    )
+            }
+        }
+    }
+}
